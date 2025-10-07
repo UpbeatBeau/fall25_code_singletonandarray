d183ab9bf8fb1b6c7a4a75d2de3fdc1f949f40a1
Upbeatbeau <70418283+UpbeatBeau@users.noreply.github.com>
2025-10-07 15:36:07 -0400

    singleton and wasd

---
+++ fall25Codeclass5/Assets/Scripts/CircleScript.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CircleScript : MonoBehaviour
+{
+    //Variables
+
+    //Make a Singleton
+    public static CircleScript instance;
+
+    public InputActionReference move;
+
+    public Rigidbody2D rb;
+
+    public float movespeed;
+
+    private Vector2 movedirection;
+
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movedirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(x: movedirection.x * movespeed, y: movedirection.y * movespeed);
+    }
+}
